8cce9e70f1c4002deca4a0983057c66801b704bd
YChaeeun <se_same5315@naver.com>
2019-05-26 07:04:54 +0900

    feat : join() 회원가입 기능 구현

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
-app = Flask(__name__)
+app          = Flask(__name__)
+app.id_count = 1
+app.users    = {}
 
 @app.route("/ping", methods=['GET'])
 def ping():
     return "pong"
+
+
+@app.route("/join", methods=['POST'])
+def join() :
+    new_user                = request.json
+    new_user['id']          = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count            = app.id_count+1
+
+    return jsonify(new_user)
